c60398a9c2e899e186f0bff70754d9d078181089
iamharshtambe <harshtambe.dev@gmail.com>
2026-02-05 15:11:58 +0530

    feat: add create bookings route

--- src/bookings/bookings.routes.ts
+++ src/bookings/bookings.routes.ts
@@ -1,10 +1,49 @@
 import { Router } from 'express';
+import { db } from '../db/db';
+import { bookings } from '../db/schema';
 
 export const bookingsRouter = Router();
 
-bookingsRouter.get('/', (req, res) => {
-  res.json({
-    message: 'Protected bookings route',
-    userId: req.user?.id,
-  });
+bookingsRouter.post('/', async (req, res) => {
+  const { carName, days, rentPerDay } = req.body;
+  const userId = req.user!.id;
+
+  if (!carName || !days || !rentPerDay) {
+    return res.status(400).json({ error: 'Invalid inputs' });
+  }
+
+  if (
+    typeof days !== 'number' ||
+    typeof rentPerDay !== 'number' ||
+    days <= 0 ||
+    days >= 365 ||
+    rentPerDay <= 0 ||
+    rentPerDay >= 2000
+  ) {
+    return res.json(400).json({ error: 'Invalid inputs' });
+  }
+
+  const [booking] = await db
+    .insert(bookings)
+    .values({ userId, carName, days, rentPerDay, status: 'booked' })
+    .returning();
+
+  if (!booking) {
+    return res.status(404).json({ error: 'Booking not found' });
+  }
+
+  const totalCost = days * rentPerDay;
+
+  try {
+    return res.status(201).json({
+      booking: booking.id,
+      carName: booking.carName,
+      rentPerDay: booking.rentPerDay,
+      totalCost,
+      status: booking.status,
+      createdAt: booking.createdAt,
+    });
+  } catch (e) {
+    res.status(500).json({ error: 'Internal server error' });
+  }
 });
