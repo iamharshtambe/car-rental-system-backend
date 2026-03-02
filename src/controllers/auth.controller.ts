cdd2eb099c4395646caad6457d4a3e103b050a81
iamharshtambe <harshtambe.dev@gmail.com>
2026-03-02 09:50:16 +0530

    ✨ Add login route

--- src/controllers/auth.controller.ts
+++ src/controllers/auth.controller.ts
@@ -1,7 +1,8 @@
 import type { Request, Response } from 'express';
-import { signupSchema } from '../validators/auth.validator';
+import { loginSchema, signupSchema } from '../validators/auth.validator';
 import { prisma } from '../lib/prisma';
-import { hashPassword } from '../utils/hash';
+import { comparePassword, hashPassword } from '../utils/hash';
+import { generatetoken } from '../utils/jwt';
 
 export async function signup(req: Request, res: Response) {
   const parsed = signupSchema.safeParse(req.body);
@@ -38,6 +39,50 @@ export async function signup(req: Request, res: Response) {
     message: 'User created successfully',
     data: {
       userId: user.id,
+    },
+  });
+}
+
+export async function login(req: Request, res: Response) {
+  const parsed = loginSchema.safeParse(req.body);
+
+  if (!parsed.success) {
+    return res.status(400).json({
+      success: false,
+      message: parsed.error.issues.map((issue) => issue.message),
+      data: null,
+    });
+  }
+
+  const { username, password } = parsed.data;
+
+  const user = await prisma.user.findUnique({ where: { username } });
+
+  if (!user) {
+    return res.status(401).json({
+      success: false,
+      message: 'Invalid credentials',
+      data: null,
+    });
+  }
+
+  const isPasswordValid = await comparePassword(password, user.password);
+
+  if (!isPasswordValid) {
+    return res.status(401).json({
+      success: false,
+      message: 'Invalid credentials',
+      data: null,
+    });
+  }
+
+  const token = generatetoken(user.id, user.username);
+
+  return res.status(200).json({
+    success: true,
+    message: 'User logged in successfully',
+    data: {
+      token,
     },
   });
 }
